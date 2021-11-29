b66b9a20ee00a7884e361f8c64028087f6cb0c72
NehaBhat-Git <89778970+NehaBhat-Git@users.noreply.github.com>
2021-11-30 00:08:00 +0530

    pattern 6

---
+++ pattern6.c
@@ -0,0 +1,27 @@
+/* 
+	*
+   ** 
+  ***
+ ****
+*****
+ */
+ 
+#include<stdio.h>
+int main()
+{
+	int row,col,n,s;
+	scanf("%d",&n);
+	for(row=1;row<=n;row++)
+	{
+		for(s=1;s<=n-row;s++)
+		{
+			printf(" ");
+		}
+		for(col=1;col<=row;col++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
