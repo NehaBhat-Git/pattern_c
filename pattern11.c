c83a93ed477282f41b92ff3cc9fc6c5aac1af8b7
NehaBhat-Git <89778970+NehaBhat-Git@users.noreply.github.com>
2021-11-30 21:35:29 +0530

    updated pattern_11

---
+++ pattern11.c
@@ -0,0 +1,27 @@
+/*
+  
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+ */
+ #include<stdio.h>
+ int main()
+ {
+ 	int row,col,n,sp;
+ 	scanf("%d",&n);
+ 	for(row=1;row<=n;row++)
+	 {
+	 	for(sp=1;sp<row;sp++)
+		 {
+		 	printf(" ");	
+		 }
+		 for(col=n-row+1;col>=1;col--)
+		 {
+		 	printf("* ");	
+		 }
+		 printf("\n");	
+	 }	
+ 	return 0;
+ }
