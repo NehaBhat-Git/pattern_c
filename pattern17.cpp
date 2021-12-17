e76197feaaa6304d7c76bbb539f985a13bde0d89
NehaBhat-Git <89778970+NehaBhat-Git@users.noreply.github.com>
2021-12-17 10:39:43 +0530

    pattern17.cpp

---
+++ pattern17.cpp
@@ -0,0 +1,26 @@
+/* 
+      4 3 2 1 
+      4 3 2 1
+      4 3 2 1
+      4 3 2 1
+*/
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	
+	int i=1;
+	while(i<=n)
+	{
+		int j=1;
+		while(j<=n)
+		{
+			cout<<(n-j+1)<<" ";
+			j++;
+		}
+		cout<<endl;
+		i++;
+	}
+return 0;
+}
